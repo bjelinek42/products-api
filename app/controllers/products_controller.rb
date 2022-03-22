5efee94c55ba167271f78b3d05d592a28131ec08
ben <bjelinek42@gmail.com>
2022-03-22 15:41:14 -0600

    create, update, delete actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,30 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params[:id])
     render json: product
   end
+
+  def create
+    product = Product.new(name: params[:name], price: params[:price], description: params[:description])
+    if product.save
+      render json: product
+    else
+      render json: {message: product.errors.full_messages}
+    end
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name]
+    product.price = params[:price]
+    product.description = params[:description]
+    if product.save
+      render json: product
+    else
+      render json: {message: product.errors.full_messages}
+    end
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.delete
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
-  delete "/products/:id" => "products#delete"
-  
+  delete "/products/:id" => "products#destroy"
+
 end
