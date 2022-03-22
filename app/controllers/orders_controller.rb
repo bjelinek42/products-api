d04c32622c4e0b376e97b61567be8f9a1829fa09
ben <bjelinek42@gmail.com>
2022-03-22 17:14:06 -0600

    subtotal, tax, and total create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,14 +10,18 @@ class OrdersController < ApplicationController
   end
   
   def create
+    product = Product.find_by(id: params[:product_id])
+    subtotal = params[:quantity].to_i * product.price
+    tax = subtotal * 0.09
+    total = subtotal + tax
     order = Order.new(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total])
+      subtotal: subtotal,
+      tax: tax,
+      total: total)
     order.save
-    render json: order
+    render json: order.as_json
   end 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   has_many :images
+
+  def tax
+    tax = subtotal * 0.09
+    tax = tax.round(2)
+  end
+
+  def total
+    total = subtotal + tax
+  end
 end
